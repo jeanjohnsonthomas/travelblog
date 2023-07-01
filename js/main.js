cb5d9cef32e407cfe52383a0e5c5cde4330fc7bf
unknown <jeanjohnsonthomasofficial@gmail.com>
2023-07-01 05:26:26 -0700

    deploy v1

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-
-toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('responsive');
-    toggleButton.classList.toggle('active');
-});
